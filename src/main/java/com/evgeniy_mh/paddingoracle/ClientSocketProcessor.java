76d907d94600c47ea73b17e13a7326bb53b64990
evgeniy-mh <djpon94@gmail.com>
2018-11-14 16:59:01 +0300

    refactor

--- src/main/java/com/evgeniy_mh/paddingoracle/ClientSocketProcessor.java
+++ src/main/java/com/evgeniy_mh/paddingoracle/ClientSocketProcessor.java
@@ -5,7 +5,6 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.InetAddress;
 import java.net.Socket;
 import java.util.concurrent.BlockingQueue;
 import java.util.logging.Level;
@@ -14,6 +13,11 @@ import java.util.logging.Logger;
 public class ClientSocketProcessor implements Runnable {
 
     private final BlockingQueue<String> messageQueue;
+    //private DataInputStream in;
+    private DataOutputStream out;
+    InputStream sin;
+    OutputStream sout;
+    Socket socket;
 
     ClientSocketProcessor(BlockingQueue<String> messageQueue) {
         this.messageQueue = messageQueue;
@@ -21,11 +25,7 @@ public class ClientSocketProcessor implements Runnable {
 
     @Override
     public void run() {
-        try {
-            initClient();
-        } catch (IOException ex) {
-            Logger.getLogger(ClientSocketProcessor.class.getName()).log(Level.SEVERE, null, ex);
-        }
+
     }
 
     private void putMessage(String message) {
@@ -36,19 +36,41 @@ public class ClientSocketProcessor implements Runnable {
         }
     }
 
-    private void initClient() throws IOException {
-        int serverPort = 55555;
-        String address = "127.0.0.1";
+    public void sendFileToServer(byte[] b) {
+        try {
+            putMessage("Connecting to Server...");
+            int serverPort = 55555;
+            String address = "127.0.0.1";
 
-        Socket socket = new Socket(address, serverPort);
+            socket = new Socket(address, serverPort);
+            sin = socket.getInputStream();
+            sout = socket.getOutputStream();
+            //in = new DataInputStream(sin);
+            out = new DataOutputStream(sout);
 
-        InputStream sin = socket.getInputStream();
-        OutputStream sout = socket.getOutputStream();
-        DataInputStream in = new DataInputStream(sin);
-        DataOutputStream out = new DataOutputStream(sout);
+            out.writeUTF("new file");
+            out.writeLong(b.length);
+            sout.write(b);
+            sout.flush();
+            putMessage("Done sending file");
+            
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLController.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            stopSocket();
+        }
 
-        out.writeUTF("Hello"); // отсылаем введенную строку текста серверу.
-        out.flush(); // заставляем поток закончить передачу данных.
+        stopSocket();
     }
 
+    public void stopSocket() {
+        try {
+            socket.close();
+            sin.close();
+            sout.close();
+            out.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ClientSocketProcessor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
--- src/main/java/com/evgeniy_mh/paddingoracle/FXMLController.java
+++ src/main/java/com/evgeniy_mh/paddingoracle/FXMLController.java
@@ -1,33 +1,87 @@
 package com.evgeniy_mh.paddingoracle;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.animation.AnimationTimer;
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.SimpleLongProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 public class FXMLController {
 
-    private MainApp mainApp;
     final BlockingQueue<String> messageQueue = new ArrayBlockingQueue<>(1);
 
+    private MainApp mainApp;
+    private FileChooser fileChooser = new FileChooser();
+    private Stage stage;
+
+    private File encryptedFile;
+
     @FXML
-    Button testButton;
+    TextArea ClientOutputTextArea;
+    @FXML
+    Button openFileButton;
+    @FXML
+    Button startDecodeButton;
 
     public void setMainApp(MainApp mainApp) {
         this.mainApp = mainApp;
     }
 
     public void initialize() {
-        testButton.setOnAction(event -> {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Test");
-            alert.showAndWait();
-        });
+        try {
+            fileChooser.setInitialDirectory(new File(MainApp.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath()).getParentFile());
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(FXMLController.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
-        ClientSocketProcessor processor = new ClientSocketProcessor(messageQueue);
-        Thread client = new Thread(processor);
+        ClientSocketProcessor clientSocket = new ClientSocketProcessor(messageQueue);
+        Thread client = new Thread(clientSocket);
         client.setDaemon(true);
         client.start();
+
+        openFileButton.setOnAction(event -> {
+            encryptedFile = openFile();
+        });
+
+        startDecodeButton.setOnAction(event -> {
+            try {
+                clientSocket.sendFileToServer(Files.readAllBytes(encryptedFile.toPath()));
+            } catch (IOException ex) {
+                Logger.getLogger(FXMLController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+
+        final LongProperty lastUpdate = new SimpleLongProperty();
+        final long minUpdateInterval = 0;
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                if (now - lastUpdate.get() > minUpdateInterval) {
+                    final String message = messageQueue.poll();
+                    if (message != null) {
+                        ClientOutputTextArea.appendText(message + "\n");
+                    }
+                    lastUpdate.set(now);
+                }
+            }
+        };
+        timer.start();
+    }
+
+    private File openFile() {
+        File file = fileChooser.showOpenDialog(stage);
+        return file;
     }
 }
