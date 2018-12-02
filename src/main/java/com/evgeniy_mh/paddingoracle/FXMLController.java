dae8e86fb52b31b4e5a07c7f931aefd9d3c3d429
evgeniy-mh <djpon94@gmail.com>
2018-12-02 13:16:30 +0300

    added decryption for all blocks except IV block

--- src/main/java/com/evgeniy_mh/paddingoracle/FXMLController.java
+++ src/main/java/com/evgeniy_mh/paddingoracle/FXMLController.java
@@ -1,8 +1,10 @@
 package com.evgeniy_mh.paddingoracle;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,45 +22,45 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class FXMLController {
-    
+
     final int AES_BLOCK_SIZE = 16;
     final BlockingQueue<String> messageQueue = new ArrayBlockingQueue<>(1);
-    
+
     private MainApp mainApp;
     private FileChooser fileChooser = new FileChooser();
     private Stage stage;
-    
+
     private File encryptedFile;
-    
+
     @FXML
     TextArea ClientOutputTextArea;
     @FXML
     Button openFileButton;
     @FXML
     Button startDecodeButton;
-    
+
     public void setMainApp(MainApp mainApp) {
         this.mainApp = mainApp;
     }
-    
+
     public void initialize() {
         try {
             fileChooser.setInitialDirectory(new File(MainApp.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath()).getParentFile());
         } catch (URISyntaxException ex) {
             Logger.getLogger(FXMLController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         openFileButton.setOnAction(event -> {
             encryptedFile = openFile();
         });
-        
+
         startDecodeButton.setOnAction(event -> {
             SendFileToServer();
         });
     }
-    
+
     private void SendFileToServer() {
-        
+
         ArrayList<byte[]> paddings = new ArrayList<>();
         for (int i = 1; i <= AES_BLOCK_SIZE; i++) {
             byte[] p = new byte[i];
@@ -70,11 +72,11 @@ public class FXMLController {
         /*paddings.forEach((padding) -> {
             debugPrintByteArray("pad", padding);
         });*/
-        
+
         if (encryptedFile == null) {
             return;
         }
-        
+
         byte[] fileToSend = null;
         try {
             fileToSend = Files.readAllBytes(encryptedFile.toPath());
@@ -91,16 +93,88 @@ public class FXMLController {
             fileBlocks.add(buff);
             debugPrintByteArray("file block #" + i, fileBlocks.get(i));
         }
-        
-        byte[] I2 = new byte[AES_BLOCK_SIZE];
+
+        for (int i = 1; i < blocksCount; i++) {
+            byte[] I2 = new byte[AES_BLOCK_SIZE];
+            int I2cnt = AES_BLOCK_SIZE - 1;
+
+            for (int b = 0; b < 16; b++) { //по байтам
+                for (int g = 0; g < 256; g++) { //по 1 байту
+                    //C1`
+                    //byte[] C1d = fileBlocks.get(blocksCount - 2).clone();
+                    byte[] C1d = new byte[AES_BLOCK_SIZE];
+
+                    byte[] Pad = paddings.get(b).clone();
+                    System.arraycopy(Pad, 0, C1d, AES_BLOCK_SIZE - Pad.length, Pad.length);//C1..|..Pad
+
+                    //System.out.println("-----------------------------------------------------------------");
+                    //debugPrintByteArray("c1, j=" + j, C1d);
+                    C1d[AES_BLOCK_SIZE - Pad.length] = (byte) (C1d[AES_BLOCK_SIZE - Pad.length] ^ g);//01^j
+                    if (b != 0) {
+                        for (int p = 0; p < AES_BLOCK_SIZE; p++) {
+                            C1d[p] = (byte) (C1d[p] ^ I2[p]);
+                        }
+                    }
+
+                    //debugPrintByteArray("c1d, j=" + j, C1d);
+                    byte[] tempFile = new byte[AES_BLOCK_SIZE * 2];
+                    byte[] C2 = fileBlocks.get(i);
+
+                    System.arraycopy(C1d, 0, tempFile, 0, AES_BLOCK_SIZE);
+                    System.arraycopy(C2, 0, tempFile, AES_BLOCK_SIZE, AES_BLOCK_SIZE); //C1` + C2
+
+                    //debugPrintByteArray("C2", fileBlocks.get(blocksCount-1));
+                    //debugPrintByteArray("C1` + C2", tempFile);
+                    Callable<Integer> callable = new FileSender(tempFile);
+                    FutureTask<Integer> ftask = new FutureTask<>(callable);
+                    Thread thread = new Thread(ftask);
+                    thread.start();
+
+                    int response = 0;
+                    try {
+                        response = ftask.get();
+                    } catch (InterruptedException | ExecutionException ex) {
+                        Logger.getLogger(FXMLController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                    if (response == 200) {
+                        putMessage("OK, j=" + g);
+                        System.out.println("j=" + g);
+                        I2[I2cnt--] = (byte) g;
+
+                        System.out.println("-------------------------------------------------");
+                        debugPrintByteArray("I2", I2);
+                        break;
+                    }
+                }
+            }
+
+            byte[] C1 = fileBlocks.get(i - 1).clone();
+            for (int p = 0; p < AES_BLOCK_SIZE; p++) {
+                C1[p] = (byte) (C1[p] ^ I2[p]);
+            }
+            debugPrintByteArray("C1", C1);
+            File out = new File("/home/evgeniy/Files/Downloads/test/out");
+            try {
+                out.createNewFile();
+
+                FileOutputStream fos = new FileOutputStream(out, true);
+                fos.write(C1);
+                fos.close();
+            } catch (IOException ex) {
+                Logger.getLogger(FXMLController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        /*byte[] I2 = new byte[AES_BLOCK_SIZE];
         int I2cnt = AES_BLOCK_SIZE - 1;
         
         for (int i = 0; i < 16; i++) { //по байтам
             for (int j = 0; j < 256; j++) { //по 1 байту
-                //byte[] C1 = fileBlocks.get(blocksCount - 2).clone();
-
                 //C1`
-                byte[] C1d = fileBlocks.get(blocksCount - 2).clone();
+                //byte[] C1d = fileBlocks.get(blocksCount - 2).clone();
+                byte[] C1d = new byte[AES_BLOCK_SIZE];
+
                 byte[] Pad = paddings.get(i).clone();
                 System.arraycopy(Pad, 0, C1d, AES_BLOCK_SIZE - Pad.length, Pad.length);//C1..|..Pad
 
@@ -112,11 +186,11 @@ public class FXMLController {
                         C1d[p] = (byte) (C1d[p] ^ I2[p]);
                     }
                 }
-                
+
                 //debugPrintByteArray("c1d, j=" + j, C1d);
                 byte[] tempFile = new byte[AES_BLOCK_SIZE * 2];
-                byte[] C2 = fileBlocks.get(blocksCount - 1);
-                
+                byte[] C2 = fileBlocks.get(1);
+
                 System.arraycopy(C1d, 0, tempFile, 0, AES_BLOCK_SIZE);
                 System.arraycopy(C2, 0, tempFile, AES_BLOCK_SIZE, AES_BLOCK_SIZE); //C1` + C2
 
@@ -126,43 +200,52 @@ public class FXMLController {
                 FutureTask<Integer> ftask = new FutureTask<>(callable);
                 Thread thread = new Thread(ftask);
                 thread.start();
-                
+
                 int response = 0;
                 try {
                     response = ftask.get();
                 } catch (InterruptedException | ExecutionException ex) {
                     Logger.getLogger(FXMLController.class.getName()).log(Level.SEVERE, null, ex);
                 }
-                
+
                 if (response == 200) {
                     putMessage("OK, j=" + j);
                     System.out.println("j=" + j);
                     I2[I2cnt--] = (byte) j;
-                    
+
                     System.out.println("-------------------------------------------------");
                     debugPrintByteArray("I2", I2);
                     break;
                 }
             }
         }
-        
-        byte[] C1 = fileBlocks.get(blocksCount - 2).clone();
+
+        byte[] C1 = fileBlocks.get(0).clone();
         for (int p = 0; p < AES_BLOCK_SIZE; p++) {
             C1[p] = (byte) (C1[p] ^ I2[p]);
         }
         debugPrintByteArray("C1", C1);
-        
+        File out = new File("/home/evgeniy/Files/Downloads/test/out");
+        try {
+            out.createNewFile();
+
+            FileOutputStream fos = new FileOutputStream(out, true);
+            fos.write(C1);
+            fos.close();
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLController.class.getName()).log(Level.SEVERE, null, ex);
+        }*/
     }
-    
+
     private void putMessage(String message) {
         ClientOutputTextArea.appendText(message + "\n");
     }
-    
+
     private File openFile() {
         File file = fileChooser.showOpenDialog(stage);
         return file;
     }
-    
+
     static public void debugPrintByteArray(String mes, byte[] array) {
         System.out.println(mes);
         for (int i = 0; i < array.length; i++) {
